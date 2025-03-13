60bb71f7cecfc032a67285350741ad1031216792
aapsi <aapsikhaira98@gmail.com>
2025-03-13 11:19:01 +0530

    Add overflow checks for ConvertToNegative contract to handle edge cases in conversion

--- src/ConvertToNegative.sol
+++ src/ConvertToNegative.sol
@@ -8,5 +8,17 @@ contract ConvertToNegative {
         // return -x
         // if -x cannot be computed, revert with "TooBig()"
         // do not hardcode any constants
+
+        // int256 range => -2 ** 255 to 2 ** 255 - 1
+        // uint256 range => 0 to 2 ** 256 -1
+        if(x > (uint256(type(int256).max) + 1)) revert TooBig();
+        // if x = 2 ** 255 then we can't do the conversion int256(2 ** 255) because it crosses teh range so we have to handle it separately
+        if(x == uint256(type(int256).max) + 1) {
+            return type(int256).min;
+        }
+        return -int256(x);
+
+        // ASK QUESTION
+        
     }
 }
