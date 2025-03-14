0b462b3a77d2cf99652218e4cca3a34fac009b9b
aapsi <aapsikhaira98@gmail.com>
2025-03-14 10:11:19 +0530

    Implement multiplication functions in Multiplication and MultiplyConstant contracts; optimize with assembly and pure visibility.

--- src/Multiplication.sol
+++ src/Multiplication.sol
@@ -2,8 +2,28 @@
 pragma solidity 0.8.28;
 
 contract Multiplication {
+
+    // [PASS] test_multiply_case1() (gas: 10048)
+    // [PASS] test_multiply_case2() (gas: 10006)
+    // [PASS] test_multiply_case3() (gas: 9778)
     function multiply(uint8 rewards, uint8 numDays) public pure returns (uint256 totalRewards) {
         // TODO: Implement the logic to calculate the total rewards, which is
         // the number of rewards multiplied by the number of days.
+        totalRewards = uint256(rewards) * numDays;
+
+    }
+
+
+    // [PASS] test_multiply_case1() (gas: 9826)
+    // [PASS] test_multiply_case2() (gas: 9784)
+    // [PASS] test_multiply_case3() (gas: 9556)
+    function multiply2(uint8 rewards, uint8 numDays) public pure returns (uint256 totalRewards) {
+        assembly {
+            let step := mul(rewards, numDays)
+            if slt(step, 0) {
+                totalRewards:= sub(0,step)
+            }
+            totalRewards := step
+        }
     }
 }
--- src/MultiplyConstant.sol
+++ src/MultiplyConstant.sol
@@ -2,7 +2,41 @@
 pragma solidity 0.8.28;
 
 contract MultiplyConstant {
+
+    // [PASS] test_multiply_case1() (gas: 9411)
+    // [PASS] test_multiply_case2() (gas: 9369)
+    // [PASS] test_multiply_case3() (gas: 9267)
+    function multiply2(uint16 x) public pure returns (uint256 fiveTimesX) {
+        // TODO: Implement the logic to calculate the five times of x.
+        assembly {
+            fiveTimesX := mul(x,5)
+        }
+    }
+
+    // [PASS] test_multiply_case1() (gas: 9657)
+    // [PASS] test_multiply_case2() (gas: 9615)
+    // [PASS] test_multiply_case3() (gas: 9513)
+    function multiply3(uint16 x) public pure returns (uint256 fiveTimesX) {
+        // TODO: Implement the logic to calculate the five times of x.
+        return uint256(x) * 5;
+    }
+
+    // [PASS] test_multiply_case1() (gas: 9631)
+    // [PASS] test_multiply_case2() (gas: 9589)
+    // [PASS] test_multiply_case3() (gas: 9487)
+    function multiply4(uint16 x) public pure returns (uint256 fiveTimesX) {
+        // TODO: Implement the logic to calculate the five times of x.
+        return (uint256(x) << 2) + uint256(x);
+    }
+
+    // [PASS] test_multiply_case1() (gas: 9459)
+    // [PASS] test_multiply_case2() (gas: 9417)
+    // [PASS] test_multiply_case3() (gas: 9315)
     function multiply(uint16 x) public pure returns (uint256 fiveTimesX) {
         // TODO: Implement the logic to calculate the five times of x.
+        assembly {
+            fiveTimesX := add(shl(2,x), x)
+        }
     }
+
 }
